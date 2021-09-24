76aa90990036bfe5f0bdac3d5073ec8074e3d033
Christian Llontop <chrisllontop@icloud.com>
2021-09-24 17:25:06 -0500

    feat: add persist in sessionStorage

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
   },
   extends: [
     'airbnb-base',
+    'plugin:import/typescript',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -15,5 +16,6 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'import/extensions': 'off',
   },
 };
